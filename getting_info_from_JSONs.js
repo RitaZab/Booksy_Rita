5c00469d940822e70d55a88282831e681debc793
Rita <rm.zaborowska@gmail.com>
2023-04-05 19:28:01 +0100

    Adding loops

--- getting_info_from_JSONs.js
+++ getting_info_from_JSONs.js
@@ -18,101 +18,129 @@ async function addingValues(id, company_name, country, voivodeship, city, county
     
     console.log(`Value ${company_name} set on ${id}`)
 }
-  
 
-fs.readFile("Businesses/10089.json", "utf8", (err, jsonString) => {
-  if (err) {
-    console.log("Error reading file from disk:", err);
-    return;
-  }
-  try {
-      const company = JSON.parse(jsonString);
-      //console.log(company)
-       // => "Customer address is: Infinity Loop Drive"
-      let id = company.business.id
-      let company_name = company.business.name
-      let country = company.business.regions[0].full_name
-      let voivodeship = company.business.regions[1].full_name
-      let city = company.business.regions[2].full_name
-      let countyRAW = company.business.regions[3].name
-      let county = countyRAW.split(" ")[1]
-      
-    let addressRAW = company.business.location.address
-    //generating post_code//
-    let post_codeRAW1 = addressRAW.match(regex1)
-    //console.log(post_codeRAW1[0])
-    let post_codeRAW2 = addressRAW.match(regex2)
-    //console.log(post_codeRAW2[0])
-    let post_code = post_codeRAW1[0] + "-" + post_codeRAW2
-    
+for (let x=1;x<2000;x++){ //main loop
+  const max=200000
+  randomID= Math.floor(Math.random()*max)
+  console.log(randomID)
 
-    let address = addressRAW.split(",")[0]
-    let offerCategory = company.business.service_categories
-    let countCategories = Object.keys(offerCategory).length
-    let offerListToConvert = []
-   let count
-    for (let y = 0; y < countCategories; y++){
-      var category = company.business.service_categories[y].name
-      //console.log(category)
-      let offer = company.business.service_categories[y].services
-       count= Object.keys(offer).length
-      let offerListRAW = []
-    
-    for (let x = 0; x < count; x++){
-      var singleOffer = company.business.service_categories[y].services[x].name
-      let variantList=company.business.service_categories[y].services[x].variants
-      //console.log(company.business.service_categories[y].services[x].variants)
-      let variantsCount = Object.keys(variantList).length
-      //console.log(variantsCount)
-      let variantsList=[]
-      for (let z = 0; z < variantsCount; z++) { 
-        let singleVariant = company.business.service_categories[y].services[x].variants[z].label
-        //console.log(singleVariant)
-        variantsList.push(singleVariant)
-        
-      }
-      //console.log(variantsList)
-      singleOffer=singleOffer+" ( "+variantsList+" ) "
-      offerListRAW.push(singleOffer)
+let path=`/var/bots/booksy_4th/businesses/${randomID}.json`
+console.log(path)
+let fileDownloaded
+try{
+  if(fs.existsSync(path)){
+      console.log("It exists")
+      fileDownloaded=true // here pass the code
+      console.log(fileDownloaded)}
       
+      if(fileDownloaded==true){
+        console.log("Sprawdz jsona")
       
-    }offerListToConvert.push(`[${category}: ${offerListRAW}]`)
-    }
+      fs.readFile(path, "utf8", (err, jsonString) => {
+        console.log(path)
+        console.log('Trying to get JSON')
+        try {
+            const company = JSON.parse(jsonString);
+            //console.log(company)
+             // => "Customer address is: Infinity Loop Drive"
+            let id = company.business.id
+            let company_name = company.business.name
+            let country = company.business.regions[0].full_name
+            let voivodeship = company.business.regions[1].full_name
+            let city = company.business.regions[2].full_name
+            let countyRAW = company.business.regions[3].name
+            let county = countyRAW.split(" ")[1]
+            
+          let addressRAW = company.business.location.address
+          //generating post_code//
+          let post_codeRAW1 = addressRAW.match(regex1)
+          //console.log(post_codeRAW1[0])
+          let post_codeRAW2 = addressRAW.match(regex2)
+          //console.log(post_codeRAW2[0])
+          let post_code = post_codeRAW1[0] + "-" + post_codeRAW2
+          
+      
+          let address = addressRAW.split(",")[0]
+          let offerCategory = company.business.service_categories
+          let countCategories = Object.keys(offerCategory).length
+          let offerListToConvert = []
+         let count
+          for (let y = 0; y < countCategories; y++){
+            var category = company.business.service_categories[y].name
+            //console.log(category)
+            let offer = company.business.service_categories[y].services
+             count= Object.keys(offer).length
+            let offerListRAW = []
+          
+          for (let x = 0; x < count; x++){
+            var singleOffer = company.business.service_categories[y].services[x].name
+            let variantList=company.business.service_categories[y].services[x].variants
+            //console.log(company.business.service_categories[y].services[x].variants)
+            let variantsCount = Object.keys(variantList).length
+            //console.log(variantsCount)
+            let variantsList=[]
+            for (let z = 0; z < variantsCount; z++) { 
+              let singleVariant = company.business.service_categories[y].services[x].variants[z].label
+              //console.log(singleVariant)
+              variantsList.push(singleVariant)
+              
+            }
+            //console.log(variantsList)
+            singleOffer=singleOffer+" ( "+variantsList+" ) "
+            offerListRAW.push(singleOffer)
+            
+            
+          }offerListToConvert.push(`[${category}: ${offerListRAW}]`)
+          }
+      
+          
+          let offerListReady = offerListToConvert.toString()
+          let about = company.business.description
+          let noOfReview = company.business.reviews_count
+          let reviewRankRAW = company.business.reviews_rank
+          let reviewRank = reviewRankRAW.toFixed(2)
+          let telephone = company.business.phone
+          let website = company.business.website
+          let facebook = company.business.facebook_link
+          let instagram = company.business.instagram_link
+          let booksyLink = 'https://booksy.com/pl-pl/' + company.business.url
+          let status="done"
+            console.log(count)
+            console.log(`Id: ${id}
+      Company name: ${company_name}
+      Country: ${country}
+      Voivodeship: ${voivodeship}
+      City: ${city}
+      County: ${county}
+      ${post_code}
+      ${address}
+      telephone: ${telephone}
+      website: ${website}
+      facebook: ${facebook}
+      instagram:${instagram}
+      Offer: 
+      ${offerListReady}
+      About:
+      ${about}
+      Review: rating: ${reviewRank} ,number of reviews ->${noOfReview}
+      Booksy link: ${booksyLink}`
+          )
+          addingValues(id, company_name, country, voivodeship, city, county, post_code, address, telephone, website, facebook, instagram, offerListReady, about, reviewRank, noOfReview, booksyLink, status)
+        } catch (err) {
+          console.log("Error parsing JSON string:", err);
+             }})}
 
-    
-    let offerListReady = offerListToConvert.toString()
-    let about = company.business.description
-    let noOfReview = company.business.reviews_count
-    let reviewRankRAW = company.business.reviews_rank
-    let reviewRank = reviewRankRAW.toFixed(2)
-    let telephone = company.business.phone
-    let website = company.business.website
-    let facebook = company.business.facebook_link
-    let instagram = company.business.instagram_link
-    let booksyLink = 'https://booksy.com/pl-pl/' + company.business.url
-    let status="done"
-      console.log(count)
-      console.log(`Id: ${id}
-Company name: ${company_name}
-Country: ${country}
-Voivodeship: ${voivodeship}
-City: ${city}
-County: ${county}
-${post_code}
-${address}
-telephone: ${telephone}
-website: ${website}
-facebook: ${facebook}
-instagram:${instagram}
-Offer: 
-${offerListReady}
-About:
-${about}
-Review: rating: ${reviewRank} ,number of reviews ->${noOfReview}
-Booksy link: ${booksyLink}`
-    )
-    addingValues(id, company_name, country, voivodeship, city, county, post_code, address, telephone, website, facebook, instagram, offerListReady, about, reviewRank, noOfReview, booksyLink, status)
-  } catch (err) {
-    console.log("Error parsing JSON string:", err);
-  }
-});
+}catch(err){
+  console.log(err)
+  fileDownloaded=false
+}}
+
+
+
+      
+
+
+
+
+
+
